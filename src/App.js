97606f3c14bd37e8eb93eb3eec01db22ac273472
Grant Ramsay <grant@gnramsay.com>
2022-07-21 15:08:40 +0100

    completed Freecodecamp weather app
    API keys removed and added to .env file

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+import CurrentWeather from "./components/current-weather/CurrentWeather";
+import Forecast from "./components/forecast/Forecast";
+import Search from "./components/search/Search";
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
+
+import "./App.css";
+
+console.log(String(WEATHER_API_KEY));
+
+function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = searchData => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      // eslint-disable-next-line max-len
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=38d4bbbb395a21580a63a047ce43594f&units=metric`
+    );
+    const forecastFetch = fetch(
+      // eslint-disable-next-line max-len
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY.replaceAll(
+        // eslint-disable-next-line quotes
+        '"',
+        ""
+      )}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async response => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  console.log(currentWeather);
+  console.log(forecast);
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/forecast/Forecast.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+
+import "./Forecast.css";
+
+const WEEK_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const Forecast = ({ data }) => {
+  const dayInWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInWeek)
+  );
+
+  return (
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    className="icon-small"
+                    src={`icons/${item.weather[0].icon}.png`}
+                    alt="weather"
+                  />
+                  <label className="day">{forecastDays[idx]}</label>
+                  <label className="description">
+                    {item.weather[0].description}
+                  </label>
+                  <label className="min-max">
+                    {Math.round(item.main.temp_min)}&#8451; /{" "}
+                    {Math.round(item.main.temp_max)}&#8451;
+                  </label>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label>Pressure</label>
+                  <label>{item.main.pressure}mB</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Humidity</label>
+                  <label>{item.main.humidity}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Clouds</label>
+                  <label>{item.clouds.all}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Wind Speed</label>
+                  <label>{item.wind.speed} m/s</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Sea Level</label>
+                  <label>{item.main.sea_level}mB</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Feels Like</label>
+                  <label>{Math.round(item.main.feels_like)}&#8451;</label>
+                </div>
+              </div>
+            </AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default Forecast;
